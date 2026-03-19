c3540442826f0739c11b2f5064fc159a5b03a752
kennedyGama7 <kennedyempregos@gmail.com>
2026-03-19 15:50:46 -0300

    Add date formatting script in formato_data.py

---
+++ formato_data.py
@@ -0,0 +1,9 @@
+print('='*30)
+print(' formatando de datas ')
+print('='*30)
+
+dia = int(input(' Dia: '))
+mes = int(input(' Mês: '))
+ano = int(input(' Ano: '))
+
+print(f'A data do seu aniversário formatada: {dia}/{mes}/{ano} ')
